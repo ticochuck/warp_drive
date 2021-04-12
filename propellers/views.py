d10ba8e2ef197e79ef4f0f327291e8ac332f1843
ticochuck <chucklivilla@gmail.com>
2021-04-12 15:07:15 -0700

    added propellers app

--- propellers/views.py
+++ propellers/views.py
@@ -1,3 +1,10 @@
+from django import forms
 from django.shortcuts import render
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Vehicle
+
+class HomePageView(ListView):
+    template_name = 'home.html'
+    model = Records
