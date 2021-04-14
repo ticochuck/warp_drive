e07f211632f064fc751029f63477c9ae19df49c1
ticochuck <chucklivilla@gmail.com>
2021-04-13 17:12:01 -0700

    import files

--- warp_drive/urls.py
+++ warp_drive/urls.py
@@ -13,12 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 from propellers import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('propellers.urls'))
-    path('', views.simple_upload)
+    path('', include('propellers.urls')),
+    path('upload/', views.upload, name='upload')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
