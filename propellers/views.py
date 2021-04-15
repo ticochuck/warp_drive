b44bedf66d7107f7bcd626d0dadded1291c3e241
ticochuck <chucklivilla@gmail.com>
2021-04-15 10:40:59 -0700

    cleaned up old import stuff

--- propellers/views.py
+++ propellers/views.py
@@ -1,49 +1,8 @@
-from django import forms
-from django.contrib import messages
-from django.http import HttpResponse
-# from django.shortcuts import render
-from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   TemplateView, UpdateView)
 
-from .models import Engine, Vehicle, Propeller
+from .models import Engine, Propeller, Vehicle
 
-
-def simple_upload(request):
-    if request.method == 'POST':
-        vehicle_resource = VehicleResource()
-        dataset = Dataset()
-        new_vehicle = request.FILES['myfile']
-
-        if not new_vehicle.name.endswith('xlsx'):
-            messages.info(request, 'wrong file format')
-            return render(request, 'propellers/upload.html')
-
-        imported_data = dataset.load(new_vehicle.read(), format='xlsx')
-        # print(imported_data)
-        # for data in imported_data:
-            # print('data es:', data)
-            # name = Vehicle(data[1])
-            # manufacturer = Vehicle(data[2])
-            # model = Vehicle(data[3])
-            # vehicle_type = Vehicle(data[4])
-            # prop_orientation = Vehicle(data[5])
-            # notes = Vehicle(data[6])
-
-            # print('hola: ', name, manufacturer, model)
-
-    #         value = Vehicle(
-    #             data[0],
-    #             data[1],
-    #             data[2],
-    #             data[3],
-    #             data[4],
-    #             data[5],
-    #             data[6]
-    #         )
-    #         print(value)
-    #         value.save()
-    # return render(request, 'propellers/upload.html')
 
 class HomePageView(ListView):
     template_name = 'propellers/home.html'
--- warp_drive/urls.py
+++ warp_drive/urls.py
@@ -13,8 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,11 +21,8 @@ from propellers import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('propellers.urls')),
-    # path('upload/', views.simple_upload, name='upload')
+    path('', include('propellers.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+
 
