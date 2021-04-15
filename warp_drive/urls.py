171ee5f571a35d9fb9270de4f3ae59da3596ad99
ticochuck <chucklivilla@gmail.com>
2021-04-15 14:13:06 -0700

    created csvs app

--- warp_drive/urls.py
+++ warp_drive/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,6 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('propellers.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
 
 
