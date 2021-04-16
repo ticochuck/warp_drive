3dda85a3a1f0103cb1bd0fbe09e5b487f179ab21
ticochuck <chucklivilla@gmail.com>
2021-04-16 10:27:53 -0700

    changes to admin site csvs

--- csvs/admin.py
+++ csvs/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from .models import Csv
 
-admin.site.register(Csv)
 
+@admin.register(Csv)
+class CsvAdmin(admin.ModelAdmin):
+    list_display = ('id', 'file_name', 'uploaded_time')
