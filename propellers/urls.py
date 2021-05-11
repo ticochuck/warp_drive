a2b8f5183b89811c298c76facc48a064dd02c71f
ticochuck <chucklivilla@gmail.com>
2021-05-11 10:50:16 -0700

    fixed import csv path

--- propellers/urls.py
+++ propellers/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import EnginePageView, HomePageView, VehiclePageView, PropellerPageView, search
+from csvs.views import upload_file_view
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('upload', upload_file_view, name='upload'),
     path('search/', search, name='search'),
     path('vehicles/', VehiclePageView.as_view(), name='vehicles'),
     path('engines/', EnginePageView.as_view(), name='engines'),
