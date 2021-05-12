dcf49dc57f1909cb95aa8abe9ed264fb7fe3b983
ticochuck <chucklivilla@gmail.com>
2021-05-12 15:35:14 -0700

    changes

--- propellers/views.py
+++ propellers/views.py
@@ -33,24 +33,19 @@ def search(request):
         form = SearchPropeller(request.POST)
         counter = 0
         if form.is_valid():
-            print('YOoOOOOOooOOOOOoooOOOO')
             engine = request.POST.get('engine_id')
             vehicle = request.POST.get('vehicle_id')
             reduction_rate = request.POST.get('reduction_ratio_rename_to_red_drive_name')
         
-        results = Propeller.objects.all().filter(engine_id = request.POST.get('engine_id'))
-        print(results)
+            results = Propeller.objects.all().filter(engine_id = request.POST.get('engine_id'))
+            results = results.filter(vehicle_id = request.POST.get('vehicle_id'))
+            print(results)
         # database_info = Propeller.objects.all()
-        # print(database_info[2])
         # for result in database_info:
         #     if result.engine_id == engine and result.vehicle_id == vehicle:
         #         counter += 1
         #         print(result.name, counter)
 
-        # for result in database_info:
-        #     if result['engine_id'] == engine and result['vehicle_id'] == vehicle and result['reduction_ratio_rename_to_red_drive_name'] == reduction_rate:
-        #         print(result)
-            
         context = {
             'results': results
         }
