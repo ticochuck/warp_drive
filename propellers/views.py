84f9a0c6724fe6b2a9ae3d3a566725da2e762abb
ticochuck <chucklivilla@gmail.com>
2021-05-13 12:13:05 -0700

    filter by rr, vehicle id, engine id

--- propellers/views.py
+++ propellers/views.py
@@ -33,13 +33,43 @@ def search(request):
         form = SearchPropeller(request.POST)
         counter = 0
         if form.is_valid():
-            engine = request.POST.get('engine_id')
-            vehicle = request.POST.get('vehicle_id')
-            reduction_rate = request.POST.get('reduction_ratio_rename_to_red_drive_name')
+            if request.POST.get('engine_id') != '' or request.POST.get('engine_id') != None: 
+                engine = request.POST.get('engine_id')
+            if request.POST.get('vehicel_id') != '' or request.POST.get('vehicel_id') != None:
+                vehicle = request.POST.get('vehicle_id')
+            if request.POST.get('reduction_ratio_rename_to_red_drive_name') != '' or request.POST.get('reduction_ratio_rename_to_red_drive_name') != None:
+                reduction_rate = request.POST.get('reduction_ratio_rename_to_red_drive_name')
         
-            results = Propeller.objects.all().filter(engine_id = request.POST.get('engine_id'))
-            results = results.filter(vehicle_id = request.POST.get('vehicle_id'))
-            print(results)
+            all_info = Propeller.objects.all()
+            
+            results = all_info
+
+            if vehicle and engine and reduction_rate:
+                results = results.filter(vehicle_id = vehicle)
+                results = results.filter(engine_id = engine)
+                results = results.filter(reduction_ratio_rename_to_red_drive_name = reduction_rate)
+            elif vehicle and engine and not reduction_rate:
+                results = results.filter(vehicle_id = vehicle)
+                results = results.filter(engine_id = engine)
+            if vehicle and reduction_rate and not engine:
+                results = results.filter(vehicle_id = vehicle)
+                results = results.filter(reduction_ratio_rename_to_red_drive_name = reduction_rate)
+            elif vehicle:
+                results = results.filter(vehicle_id = vehicle)
+            elif engine and reduction_rate:
+                results = results.filter(engine_id = engine)
+                results = results.filter(reduction_ratio_rename_to_red_drive_name = reduction_rate)
+            elif engine:
+                results = results.filter(engine_id = engine)
+            elif reduction_rate:
+                results = results.filter(reduction_ratio_rename_to_red_drive_name = reduction_rate)
+
+            # elif vehicle != '' or vehicle != None:
+            #     results = Propeller.filter(vehicle_id = vehicle)
+
+            # results = Propeller.objects.all().filter(engine_id = request.POST.get('engine_id'))
+            # results = results.filter(vehicle_id = request.POST.get('vehicle_id'))
+            # print(results)
         # database_info = Propeller.objects.all()
         # for result in database_info:
         #     if result.engine_id == engine and result.vehicle_id == vehicle:
